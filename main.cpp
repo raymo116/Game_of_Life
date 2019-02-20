64962e2ac58eaa872a91df122f80811dc1c53dc1
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-02-20 03:49:29 +0000

    Added main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "grid.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    grid myGrid;
+
+    myGrid.printGrid();
+    myGrid.printGrid();
+
+    return 0;
+}
