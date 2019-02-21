10b633394a02da41eafe838fb6fcb3a490d2ddbe
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-02-21 19:54:12 +0000

    Added logic for game rules

---
+++ gameRules.h
@@ -0,0 +1,15 @@
+#ifndef GAME_RULES
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace std {
+    class gameRules
+    {
+    public:
+        static void evaluate(int neighbors);
+    private:
+    };
+}
+#endif
