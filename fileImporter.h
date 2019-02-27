897fa5f25b3ad74b76d19bb1a507d00563fe7fd1
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-02-27 06:29:12 +0000

    Added user input and random map generation

--- fileImporter.h
+++ fileImporter.h
@@ -12,6 +12,10 @@ public:
     fileImporter(string fp);
     fileImporter();
     void openFile(string filepath, char*** currentGen, char*** nextGen, int* xSize, int* ySize);
+    void generate(char*** currentGen, char*** nextGen, int* xSize, int* ySize);
+    static void generalInit(char*** currentGen, char*** nextGen, int* xSize, int* ySize);
+    static void generateNew(char*** currentGen, char*** nextGen, int* xSize, int* ySize);
+    static void getNumber(int* currentNum, string myString, bool isInt);
     string filepath;
 private:
     char check(char character);
