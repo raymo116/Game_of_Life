30b8c2cb6e220a6f022283a69b7a9a11c013170c
Matt Raymond <m4tt.r4ymond@gmail.com>
2019-02-28 06:32:32 +0000

    Set up user-defined map importing

--- fileImporter.h
+++ fileImporter.h
@@ -11,12 +11,14 @@ class fileImporter
 public:
     fileImporter(string fp);
     fileImporter();
-    void openFile(string filepath, char*** currentGen, char*** nextGen, char*** checkGen, int* xSize, int* ySize, bool random);
+    void openFile(char*** currentGen, char*** nextGen, char*** checkGen, int* xSize, int* ySize, bool random);
     void generate(char*** currentGen, char*** nextGen, int* xSize, int* ySize);
     static void generalInit(char*** currentGen, char*** nextGen, char*** checkGen, int* xSize, int* ySize, bool random);
     static void generateNew(char*** currentGen, char*** nextGen, char*** checkGen, int* xSize, int* ySize);
     static void getNumber(int* currentNum, string myString, bool isInt);
+    static void testPath(string* path);
     string filepath;
+    string outpath;
 private:
     char check(char character);
     int parseNum(ifstream* currentStream);
