fb45d64d36afe3de78b930f8a2e88cef2effbd88
Matthew Raymond <matthewraymond@Matthews-MacBook-Pro.local>
2019-03-05 11:18:00 -0800

    Add Comments

--- grid.cpp
+++ grid.cpp
@@ -12,25 +12,15 @@
 
 using namespace std;
 
-Grid::Grid(){
-    //default is a random grid
-    FileImporter::GenerateNew(&currentgen_, &nextgen_, &checkgen_, &xsize_, &ysize_);
-
-    int delaylength = 100;
-    gennumber_ = mode_ = viewmode_ = 0;
-
-    waitms_ = (delaylength >= 0) ? delaylength:0;
-
-    ts_.tv_sec = waitms_ / kDelayTime;
-    ts_.tv_nsec = (waitms_ % kDelayTime) * pow(kDelayTime, 2);
-}
-
+// Constructor for the Grid object
+// Takes in ints for border type and viewmode, a bool for whether the grid is supposed to be random, and an in for the delay length
 Grid::Grid(int border, int view, bool random, int delaylength){
     if(!random){
+        // Open from file
         FileImporter myImporter;
-        myImporter.OpenFile(&currentgen_, &nextgen_, &checkgen_, &xsize_, &ysize_, random);
+        myImporter.OpenFile(&currentgen_, &nextgen_, &checkgen_, &xsize_, &ysize_, false);
     }
-    else
+    else // generate a new map
         FileImporter::GenerateNew(&currentgen_, &nextgen_, &checkgen_, &xsize_, &ysize_);
 
     gennumber_ = 0;
@@ -44,30 +34,30 @@ Grid::Grid(int border, int view, bool random, int delaylength){
     if(view == 2) outputfile_.open("mRaymond_cValencia.out");
 }
 
+// Destructor
 Grid::~Grid(){
+    // Delete all of the grids
     if(currentgen_){
-        /*for(int y = 0; y < ysize_; ++y)
-            for(int x = 0; x < xsize_; ++x)
-                delete[] &(currentgen_[y][x]);*/
+        for(int y = 0; y < ysize_; ++y)
+            delete[] currentgen_[y];
         delete[] currentgen_;
     }
     if(nextgen_){
-        /*for(int y = 0; y < ysize_; ++y)
-            for(int x = 0; x < xsize_; ++x)
-                delete[] &(nextgen_[y][x]);*/
+        for(int y = 0; y < ysize_; ++y)
+            delete[] nextgen_[y];
         delete[] nextgen_;
     }
     if(checkgen_){
-        /*for(int y = 0; y < ysize_; ++y)
-            for(int x = 0; x < xsize_; ++x)
-                delete[] &(checkgen_[y][x]);*/
+        for(int y = 0; y < ysize_; ++y)
+            delete[] checkgen_[y];
         delete[] checkgen_;
     }
+    // Close the output filestream
     if(outputfile_)
         outputfile_.close();
 }
 
-//will run the game specified ammount of times
+// Takes in an int for the number of generations and generates that many generations
 void Grid::Run(int times){
     for (int i = 0; i < times; ++i){
         //Print grid based on view mode
@@ -97,7 +87,7 @@ void Grid::Run(int times){
             break;
         }
 
-        //Copy every other grid into the checking spot
+        // Copy every other generation into che checkgen_ grid to check for periods of 2 (blinkers)
         if(i % 2 == 0) CopyContents(&currentgen_, &checkgen_);
 
         //Swap the grids
@@ -110,10 +100,12 @@ void Grid::Run(int times){
 
 }
 
-void Grid::PrintGrid(){ // Send to console
-    if (viewmode_ == 0 || viewmode_ == 1){
+// Prints the grid to the console
+void Grid::PrintGrid(){
+    if (viewmode_ < 2){
         //Dont jump back the first time
         if (gennumber_ > 0){
+            // Deletes the previous grid from the screen
             for (int y = 0; y < ysize_ + 1 + viewmode_; ++y){
                 cout << "\033[F";
             }
@@ -122,7 +114,8 @@ void Grid::PrintGrid(){ // Send to console
             cout << endl;
     }
 
-    cout << "Generation " << gennumber_ << kBlankSpace << endl;
+    // Prints the new contents to the screen
+    cout << "Generation " << gennumber_ << endl;
     for (int y = 0; y < ysize_; ++y){
         for (int x = 0; x < xsize_; ++x){
             cout << currentgen_[y][x] << ' ';
@@ -131,8 +124,9 @@ void Grid::PrintGrid(){ // Send to console
     }
 }
 
-void Grid::PrintGrid(ofstream* mystream){ // Send to filestream
-    (*mystream) << "Generation " << gennumber_ << kBlankSpace << endl;
+// Overload of PrintGrid that saves to a file. It takes a pointer to an ofstream
+void Grid::PrintGrid(ofstream* mystream){
+    (*mystream) << "Generation " << gennumber_ << endl;
     for (int y = 0; y < ysize_; ++y){
         for (int x = 0; x < xsize_; ++x){
             (*mystream) << currentgen_[y][x];
@@ -141,7 +135,9 @@ void Grid::PrintGrid(ofstream* mystream){ // Send to filestream
     }
 }
 
+// Checks the contents of the cells surrounding a certain xy point
 int Grid::ReturnSurrounding(int x, int y){
+    // Prevents segfault
     CheckRCError(x, y);
 
     // Keep us from having to do the math multiple times for boolean evaluations
@@ -159,10 +155,11 @@ int Grid::ReturnSurrounding(int x, int y){
             if((xscan == 0) && (yscan == 0))
                 continue;
 
+            // The current index to look at
             xtemp = x + xscan;
             ytemp = y + yscan;
 
-            //Here we switch on the mode for custom behavoir
+            // Here we switch on the mode for custom behavoir
             switch (mode_) {
                 case 0: //CLASSIC
                     ClassicReturn(ytemp, xtemp, &count);
@@ -180,31 +177,40 @@ int Grid::ReturnSurrounding(int x, int y){
 }
 
 // Checks to make sure that the rows given are valid and won't go outside of the grid
+// Takes in x and y coordinates as ints
 void Grid::CheckRCError(int x, int y){
     if(((x >= xsize_) || (x < 0)) || (y >= ysize_) || (y < 0))
         throw invalid_argument(("recieved a index outside of the grid: index "+to_string(y)+","+to_string(x)));
 }
 
+// Performs classic return. Takes yx coordinates as ints, and a pointer to the number of neighbors
 void Grid::ClassicReturn(int y, int x, int* count){
+    // If the coordinates are contained in the grid
     if(!(((x < 0) || (y < 0)) || ((x >= xsize_) || (y >= ysize_))))
+        // If the character is an X
         if(currentgen_[y][x] == 'X') (*count)++;
 }
 
+// Performs donut return. Takes yx coordinates as ints, and a pointer to the number of neighbors
 void Grid::DonutReturn(int y, int x, int* count){
-    x = (x+xsize_)%xsize_;
+    x = (x+xsize_)%xsize_; // e.g. if grid.length=5 and x=5, x=(5+5)%5 = 10%5 = 0, or the other side of the grid
     y = (y+ysize_)%ysize_;
     if(currentgen_[y][x] == 'X') (*count)++;
 }
 
+// Performs mirror return. Takes yx coordinates as ints, and a pointer to the number of neighbors
 void Grid::MirrorReturn(int y, int x, int* count){
-    x = ((x==-1) || (x==xsize_)) ? abs(x)-1 : x;
-    y = ((y==-1) || (y==ysize_)) ? abs(y)-1 : y;
+    x = ((x==-1) || (x==xsize_)) ? abs(x)-1 : x; // e.g. if grid.length=5 and x=-1, x=abs(-1)-1 = 1-1 = 0
+    y = ((y==-1) || (y==ysize_)) ? abs(y)-1 : y; // if grid.length=5 and x=6, x=abs(6)-1 = 6-1 = 5
     if(currentgen_[y][x] == 'X') (*count)++;
 }
 
-//Reads the first grid into GameRules and outputs results into second grid
+// Reads the first grid into GameRules and outputs results into second grid
+// Takes two pointers to character grids, one for the source and one for the output
 void Grid::CopyContents(char*** source, char*** destination){
     for (int y = 0; y < ysize_; ++y)
         for (int x = 0; x < xsize_; ++x)
+            // return the number of neighbors for each yx coordinate, checks the game rules to see if it's alive or not,
+            // and then saves the result to the destination grid
             (*destination)[y][x] = GameRules::Evaluate(ReturnSurrounding(x,y), (*source)[y][x]);
 }
--- grid.h
+++ grid.h
@@ -8,62 +8,69 @@
 
 using namespace std;
 
-namespace std {
-    class Grid
-    {
-        public:
-            Grid();
-            Grid(int border, int view, bool random, int delaylength);
-            ~Grid();
+class Grid
+{
+    public:
+        // Constructor for the Grid object
+        // Takes in ints for border type and viewmode, a bool for whether the grid is supposed to be random, and an in for the delay length
+        Grid(int border, int view, bool random, int delaylength);
+        // Destructor
+        ~Grid();
 
-            // Will run the game specified ammount of times
-            void Run(int times);
+        // Takes in an int for the number of generations and generates that many generations
+        void Run(int times);
+        // Prints the grid to the console
+        void PrintGrid();
+        // Overload of PrintGrid that saves to a file. It takes a pointer to an ofstream
+        void PrintGrid(ofstream* myStream);
 
-            void PrintGrid();
-            void PrintGrid(ofstream* myStream);
+        // For switching these grids back and forth via reference
+        char** addresstemp_;
+        char** currentgen_;
+        char** nextgen_;
+        char** checkgen_;
 
-            // For switching these grids back and forth via reference
-            char** addresstemp_;
-            char** currentgen_;
-            char** nextgen_;
-            char** checkgen_;
+        // The number of the current generation
+        int gennumber_;
 
-            int gennumber_;
+    private:
+        // The default delay time for the grid rendering
+        const int kDelayTime = 1000;
 
-        private:
-            const int kDelayTime = 1000;
-            const string kBlankSpace = "            ";
+        // Checks the contents of the cells surrounding a certain xy point
+        int ReturnSurrounding(int row, int column);
+        // Checks to make sure that the rows given are valid and won't go outside of the grid
+        // Takes in x and y coordinates as ints
+        void CheckRCError(int y, int x);
+        // Performs classic return. Takes yx coordinates as ints, and a pointer to the number of neighbors
+        void ClassicReturn(int x, int y, int* nC);
+        // Performs donut return. Takes yx coordinates as ints, and a pointer to the number of neighbors
+        void DonutReturn(int x, int y, int* nC);
+        // Performs mirror return. Takes yx coordinates as ints, and a pointer to the number of neighbors
+        void MirrorReturn(int x, int y, int* nC);
+        // Reads the first grid into GameRules and outputs results into second grid
+        // Takes two pointers to character grids, one for the source and one for the output
+        void CopyContents(char*** source, char*** destination);
 
-            int ReturnSurrounding(int row, int column);
+        // The x and y sizes of the grid
+        int xsize_;
+        int ysize_;
 
-            void CheckRCError(int y, int x);
+        // Game mode
+        // (0, classic), (1, donut), (2, mirror)
+        int mode_;
 
-            void ClassicReturn(int x, int y, int* nC);
-            void DonutReturn(int x, int y, int* nC);
-            void MirrorReturn(int x, int y, int* nC);
+        // View Mode
+        // (0, user-specified pause), (1, require enter), (2, file output)
+        int viewmode_;
 
-            void CopyContents(char*** source, char*** destination);
+        // Wait time in milliseconds
+        int waitms_;
+        struct timespec ts_;
+        // output filestream
+        ofstream outputfile_;
+        // filepath to export to
+        string outputfilepath_;
 
-            int xsize_;
-            int ysize_;
-
-            // 0 = classic
-            // 1 = donut
-            // 2 = mirror
-            int mode_;
-
-            // 0 = brief pause - user can specify how long
-            // 1 = require enter
-            // 2 = file output
-            int viewmode_;
-
-            // Wait time in milliseconds
-            int waitms_;
-            struct timespec ts_;
-
-            ofstream outputfile_;
-            string outputfilepath_;
-
-    };
-}
+};
 #endif
